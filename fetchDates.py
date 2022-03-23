954c121fdd4eea9f055886c63a26d402367622e3
12122096 <e12122096@student.tuwien.ac.at>
2022-03-24 00:11:28 +0100

    adding getDates and rrule-patch to get normal events/dates and recurring dates

--- fetchDates.py
+++ fetchDates.py
@@ -1,36 +1,12 @@
-from cgi import test
 from datetime import datetime, timedelta, timezone
-from itertools import count
-from lib2to3.pytree import convert
-from traceback import print_tb
 import icalendar
+import getDates
 from dateutil.rrule import *
 import math
-
 import pytz
 
-icalfile = open('calendar_uni_wichtig.ics', 'rb')
-gcal = icalendar.Calendar.from_ical(icalfile.read())
-icalfile.close()
-
-savedEntries = []
-for component in gcal.walk():
-    if component.name == "VEVENT":
-        
-        summary = component.get('summary')
-        description = component.get('description')
-        location = component.get('location')
-        startdt = component.get('dtstart').dt
-        enddt = component.get('dtend').dt
-        exdate = component.get('exdate')
-
-        #   compare datetime in calender with current time (date and year) and use only upcoming event-dates
-        # print(summary)
-        if startdt.strftime("%D") >= datetime.strftime(datetime.today(),"%D") and startdt.strftime("%Y") >= datetime.strftime(datetime.today(),"%Y"): 
-            savedEntries.append([startdt.strftime("%D %H:%M"), str(summary)])
-
-#   sort dates
-sortedEntries = sorted(savedEntries)
+#   saving dates + recurrent dates from getDates script into sortedEntries
+sortedEntries = getDates.recurrent_dates
 
 def print_dates(displayDays):
     count = 0
@@ -42,16 +18,15 @@ def print_dates(displayDays):
         #   save originalEntry at index
         originalEntry = sortedEntries[count]
 
-        #   convert string-dates into datetime-dates again
-        #   'entry' is used because of iteration (datetime would be converted into datetime (with strptime)
-        #   after second iteration --> error)
-        timeLeft = datetime.strptime(originalEntry[0], "%m/%d/%y %H:%M") - datetime.today()
+        #   converting originalEntry to string and then to date back again because weird timezone problem of new scripts
+        timeLeft = datetime.strptime(originalEntry[0].strftime("%D %H:%M"), "%m/%d/%y %H:%M") - datetime.today()
+        
         timeLeftSeconds = timeLeft.total_seconds()
         days = math.ceil((timeLeftSeconds/3600)/24)
         
         #   convert datetime to utc timestamp
         local = pytz.timezone('Europe/Vienna')
-        naive = datetime.strptime(originalEntry[0], "%m/%d/%y %H:%M")
+        naive = datetime.strptime(originalEntry[0].strftime("%D %H:%M"), "%m/%d/%y %H:%M")
         local_dt = local.localize(naive, is_dst=None)
         utc_dt = local_dt.astimezone(pytz.utc)
 
@@ -71,7 +46,4 @@ def print_dates(displayDays):
                 text = '<t:'+str(content[0])+':F>' + ' ' + content[1] + ' ' + '<t:'+str(content[0])+':R>'
                 output += text+'\n' if content != entry[len(entry)-1] else text
             return output
-
-#   TODO: weekly frequency is not recognized
-
-print_dates(21)
+# print_dates(21)
