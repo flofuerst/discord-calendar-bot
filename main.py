71bfa615b0d116f20f5d6b84610d3e6dcfd47eea
12122096 <e12122096@student.tuwien.ac.at>
2022-10-17 18:07:55 +0200

    removed log commands

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ client = commands.Bot(command_prefix='§')
 daysToDisplay = 14
 
 #   create logging-file
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+#logger = logging.getLogger('discord')
+#logger.setLevel(logging.DEBUG)
+#handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w')
+#handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+#logger.addHandler(handler)
 
 
 #   init bot
