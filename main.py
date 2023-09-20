69c930a1680632e3e6bb06b01aa0843eaf082c87
Flo Fuerst <flo.fuerst@outlook.de>
2023-09-20 21:58:29 +0200

    Add fix for multiple create events of same user

--- main.py
+++ main.py
@@ -8,16 +8,10 @@ import logging
 import createEvent
 from datetime import datetime
 
-#   TODO: make server and channel based messages (prevent declared problem where bot only writes on specified channel,
-#         regardless of server and channel of initialized message)
-#   TODO: upload memes from #memes channel?
-#   TODO: implement reminder in event creation
-#   TODO: Edit message in event creation (or after, like ',cal edit...')
-#   TODO: Create recurring event
-#   TODO: red cross reaction emoji for deleting every bot message (except event info)
 
 bot = commands.Bot(command_prefix=',cal ', intents=discord.Intents.all())
 daysToDisplay = 14
+activeMessages = set()
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
@@ -58,10 +52,10 @@ bot.help_command = MyHelp()
 @bot.event
 async def on_ready():
     logging.info('Bot ready, starting to display dates')
-    channel = bot.get_channel(id=955592796482437140)
+    channel = bot.get_channel(id=933047858880446477) # 955592796482437140
 
     global writtenMessage
-    writtenMessage = await channel.fetch_message(986402491430207529)
+    writtenMessage = await channel.fetch_message(1154135543676932129) # 986402491430207529
     await bot.change_presence(status=discord.Status.online, activity=discord.Game(',cal help'))
     task_loop.start(writtenMessage)
 
@@ -101,6 +95,8 @@ async def create(ctx, startDate, startTime, endDate, endTime, *title):
     """Used to add events to calendar\n\nSYNOPSIS:\n,cal create DD.MM.YYYY HH:MM DD.MM.YYYY HH:MM TITLE...\n\n
     example: ,cal create 17.02.2023 18:00 17.02.2023 20:10 [ANA] Anmeldung Test1"""
     global createMessage, createContext, createTitle, createStartDate, createStartTime, createEndDate, createEndTime
+
+
     # global var for create-message
     createMessage = ctx.message
 
@@ -123,8 +119,14 @@ async def create(ctx, startDate, startTime, endDate, endTime, *title):
     startT = bool(re.search(timeRegex, startTime))
     endT = bool(re.search(timeRegex, endTime))
 
+    if(createContext.author.id in activeMessages):
+        em = discord.Embed(
+            title=f"Error!", description=f"Ongoing event creation not completed yet",
+            color=ctx.author.color)
+        errMsg = await ctx.send(embed=em)
+        logging.info('Failed event creation - ongoing creation not completed')
     # only create event if specified dates (and time) are correct
-    if (startD and endD and startT and endT and createTitle != ""):
+    elif (startD and endD and startT and endT and createTitle != ""):
         em = discord.Embed(title=f"Do you really want to create this event?",
                            description=f"Title: {createTitle}\nStart date: {startDate} at {startTime}\nEnd date: {endDate} at {endTime}",
                            color=ctx.author.color)
@@ -133,6 +135,7 @@ async def create(ctx, startDate, startTime, endDate, endTime, *title):
         # global var for confirm-message
         global eventConfirm
         eventConfirm = message
+        activeMessages.add(createContext.author.id)
         await message.add_reaction("✅")
         await message.add_reaction("❌")
     # error if not correct date(s) specified
@@ -143,11 +146,6 @@ async def create(ctx, startDate, startTime, endDate, endTime, *title):
         errMsg = await ctx.send(embed=em)
         logging.info('Failed event creation - incorrect input')
 
-        # async sleep
-        # await asyncio.sleep(60)
-        # await createMessage.delete()
-        # await errMsg.delete()
-
     task_loop.start(writtenMessage)
 
 
@@ -155,9 +153,8 @@ async def create(ctx, startDate, startTime, endDate, endTime, *title):
 async def on_raw_reaction_add(payload):
     # check if same user, correct message id and correct emoji
     if (payload.user_id == createContext.author.id and payload.message_id == eventConfirm.id and payload.emoji.name == "✅"):
-        # delete create and confirm messages
+        # delete confirm message
         await eventConfirm.delete()
-        # await createMessage.delete()
 
         # build up correct datetime
         startDate = createStartDate.split('.')
@@ -176,6 +173,7 @@ async def on_raw_reaction_add(payload):
                            description=f"Following event was created:\n\nTitle: {createTitle}\nStart date: {createStartDate} at {createStartTime}\nEnd date: {createEndDate} at {createEndTime}\n\n Following command was used:\n,cal create {createStartDate} {createStartTime} {createEndDate} {createEndTime} {createTitle}",
                            color=createContext.author.color)
         infoMsg = await createContext.send(embed=em)
+        activeMessages.remove(createContext.author.id)
         logging.info('Successful event creation')
 
         # async sleep
@@ -183,14 +181,14 @@ async def on_raw_reaction_add(payload):
         # await infoMsg.delete()
     # check if same user, correct message id and correct emoji
     elif (payload.user_id == createContext.author.id and payload.message_id == eventConfirm.id and payload.emoji.name == "❌"):
-        # delete create and confirm messages
+        # delete confirm message
         await eventConfirm.delete()
-        # await createMessage.delete()
 
         em = discord.Embed(title=f"Event not created!",
                            description=f"Following command was used:\n,cal create {createStartDate} {createStartTime} {createEndDate} {createEndTime} {createTitle}",
                            color=createContext.author.color)
         infoMsg = await createContext.send(embed=em)
+        activeMessages.remove(createContext.author.id)
         logging.info('Aborted event creation')
 
         # async sleep
